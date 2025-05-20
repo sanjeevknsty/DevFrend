7fae0dd0dea077cd190fcd33bf5016fbaba60a20
sanjeevknsty <ksanjeev0359@gmail.com>
2025-05-21 00:29:18 +0530

    vite mod3 commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./components/CreateTheme.jsx";
 
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from '@emotion/react'
-import theme from './components/CreateTheme.jsx'
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <ThemeProvider theme={theme}>
-    <App />
+const container = document.getElementById("root");
+let root = container._reactRoot || createRoot(container);
+container._reactRoot = root; // Store the root instance
+
+root.render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
